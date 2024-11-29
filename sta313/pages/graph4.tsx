84b1f5122e42da8add3f833cd1ed6f555f379133
Chenxu Robin Mao <robin.mao@mail.utoronto.ca>
2024-11-28 23:06:17 -0500

    now all four pages are viewable

---
+++ sta313/pages/graph4.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Page() {
+    return (
+        <div style={{
+            textAlign: 'center',
+            marginTop: '10px'
+        }}>
+            HelloWorld!
+        </div>
+    )
+}
