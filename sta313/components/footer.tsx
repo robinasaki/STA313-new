3d7f791bbdc90afff72b86a1978308726b9464b5
Chenxu Robin Mao <robin.mao@mail.utoronto.ca>
2024-11-29 13:16:04 -0500

    added footer instruction

--- sta313/components/footer.tsx
+++ sta313/components/footer.tsx
@@ -5,18 +5,26 @@ export function Footer() {
     return (
         <div style={{
             display: 'flex',
-            justifyContent: 'center',
+            flexDirection: 'column',
+            alignItems: 'center',
             position: 'fixed',
             bottom: 0,
             width: '100%',
             background: 'linear-gradient(to right, #261b32, #461b52)',
             borderTop: '1px solid gray'
         }}>
+            <p style={{ fontSize: '20px', fontFamily: 'monospace', marginTop: '20px' }}>Click here to nevigate to:</p>
+            <div style={{
+            display: 'flex',
+            justifyContent: 'center',
+            width: '100%'
+            }}>
             <PageButton content={"Main Page"} href={"/"} /> 
             <PageButton content={"Graph 1"} href={"/graph1"} />
             <PageButton content={"Graph 2"} href={"/graph2"} />
             <PageButton content={"Graph 3"} href={"/graph3"} />
             <PageButton content={"Graph 4"} href={"/graph4"} />
+            </div>
         </div>
     );
 }
